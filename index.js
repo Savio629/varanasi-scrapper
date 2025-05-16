3840227e314c3dc3ba4a05d10768b406593cca50
Savio Dias <diassavio629@gmail.com>
2025-05-16 20:33:47 +0530

    updated

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const puppeteer = require('puppeteer');
 const { createClient } = require('@supabase/supabase-js');
-
+const { executablePath } = require('puppeteer');
+const fs = require('fs');
+const path = require('path');
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_KEY = process.env.SUPABASE_KEY;
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
